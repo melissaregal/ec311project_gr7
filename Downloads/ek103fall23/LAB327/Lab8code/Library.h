f861c7dc30c7f7869346b77db1bce21f80b6eb57
Melissa Regalado <mregal@bu.edu>
2023-12-01 11:03:37 -0500

    This code doenst work anymore :((

---
+++ Downloads/ek103fall23/LAB327/Lab8code/Library.h
@@ -0,0 +1,32 @@
+#ifndef _LIBRARY_H
+#define _LIBRARY_H 1
+
+#include <string>
+
+//DO NOT CHANGE THIS FILE
+
+
+class Library{
+public:
+    //constructor
+    //creates a library with no books
+    Library();
+
+    //Adds the given book to the library and returns zero
+    //returns false if the book already exists or if the library
+    //  already has 10 books
+    bool addBook(std::string bookName);
+
+    //Removes the given book name from the library by setting the 
+    //  corresponding string to "", and returns true
+    //returns false if the library does not have the given book
+    bool removeBook(std::string bookName);
+
+    //prints the book names
+    void print();
+
+private:
+    std::string books[10]; //book names. (by default, all are initialized with "")
+};
+
+#endif
